7eee6ff8a01debeb6e17800ea9e3f2df12e06773
Christian Persson <christian.lars.persson@gmail.com>
2019-05-17 09:49:57 +0200

    Initial commit, setup of project

---
+++ HueLit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HueLit
+//
+//  Created by Christian Persson on 2019-05-17.
+//  Copyright © 2019 Christian Persson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
