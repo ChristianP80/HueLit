97208ecb6be78909e4d8d3d39b03bf9cb4e1878c
Christian Persson <christian.lars.persson@gmail.com>
2019-05-21 23:51:48 +0200

    Add a loop and make put request every 3 seconds until pushlink is pressed

--- HueLit/ViewControllers/PushLinkViewController.swift
+++ HueLit/ViewControllers/PushLinkViewController.swift
@@ -14,30 +14,48 @@ class PushLinkViewController: UIViewController {
 
     let url = "http://192.168.1.225/api"
     let jsonBody : [String : Any] = ["devicetype" : "my_hue_app#iphone peter"]
+    var user : String?
+    var isLooping : Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkIfPushLinkIsPressed()
+        isLooping = true
+        getUser()
     }
     
-    func checkIfPushLinkIsPressed() {
-        
-        Alamofire.request(url, method: .post, parameters: jsonBody, encoding: JSONEncoding.default)
-            .responseJSON { response in
-                if response.result.isSuccess {
-                    let jsonResponse = JSON(response.result.value as Any)
-
-                    if jsonResponse[0]["success"].exists() {
-                        print("contains success")
-                    }
-                } else {
-                    print("Funkar inte")
-                    print(response.error.debugDescription)
-                }
-        }
-        
+    override func viewDidDisappear(_ animated: Bool) {
+        isLooping = false
     }
-
+    
+    func getUser() {
+        if isLooping {
+            if user == nil {
+                perform(#selector(checkIfPushLinkIsPressed), with: nil, afterDelay: 3)
+                print("User is still nil")
+            }
+        }
+    }
+    
+    @objc func checkIfPushLinkIsPressed() {
+            Alamofire.request(url, method: .post, parameters: jsonBody, encoding: JSONEncoding.default)
+                .responseJSON { response in
+                    if response.result.isSuccess {
+                        let jsonResponse = JSON(response.result.value as Any)
+                        
+                        if jsonResponse[0]["success"].exists() {
+                            print("contains success")
+                            self.user = jsonResponse[0]["success"]["username"].stringValue
+                            print(self.user!)
+                        }
+                        if jsonResponse[0]["error"].exists() {
+                            self.getUser()
+                        }
+                    } else {
+                        print("Funkar inte")
+                        print(response.error.debugDescription)
+                    }
+            }
+    }
 }
 
 
