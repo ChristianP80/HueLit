83dc2d3be206238ae66447d0b057a8c76d7600ec
Christian Persson <christian.lars.persson@gmail.com>
2019-05-25 23:22:21 +0200

    Add animation when cells load on tableview

---
+++ HueLit/Cells/RoomCell.swift
@@ -0,0 +1,20 @@
+//
+//  RoomCell.swift
+//  HueLit
+//
+//  Created by Christian Persson on 2019-05-25.
+//  Copyright © 2019 Christian Persson. All rights reserved.
+//
+
+import UIKit
+
+class RoomCell: UITableViewCell {
+
+
+    @IBOutlet weak var roomTytpeImage: UIImageView!
+    @IBOutlet weak var roomNameLabel: UILabel!
+    @IBOutlet weak var lightsInfoLabel: UILabel!
+    @IBOutlet weak var lightSwitch: UISwitch!
+    
+    
+}
--- HueLit/ViewControllers/ConnectBridgeViewController.swift
+++ HueLit/ViewControllers/ConnectBridgeViewController.swift
@@ -20,29 +20,63 @@ class ConnectBridgeViewController: UIViewController, UITableViewDelegate, UITabl
         super.viewDidLoad()
         bridgeTableView.delegate = self
         bridgeTableView.dataSource = self
+        bridgeTableView.backgroundColor = UIColor.black
+        self.view.backgroundColor = UIColor.black
         print(bridges)
+        animateTable()
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        //
-        return 1
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return bridges.count
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //
         return bridges.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "bridgeCell", for: indexPath)
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "bridgeCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bridgeCell", for: indexPath)
+        cell.layer.borderWidth = 5.0
+        
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.cornerRadius = cell.frame.height / 4
+        cell.backgroundColor = UIColor.lightGray
         cell.textLabel?.text = bridges[indexPath.row].ip
         cell.detailTextLabel?.text = bridges[indexPath.row].id
-        print(bridges[indexPath.row].id)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //
+        
+        performSegue(withIdentifier: "tableViewPushLink", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tableViewPushLink" {
+            let pushLinkVC = segue.destination as! PushLinkViewController
+            let index = bridgeTableView.indexPathForSelectedRow?.row
+            pushLinkVC.bridge = bridges[index!]
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func animateTable(){
+        bridgeTableView.reloadData()
+        let cells = bridgeTableView.visibleCells
+        
+        let tableViewHeight = bridgeTableView.bounds.size.height
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: Double(delayCounter) * 0.05,usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [.curveEaseIn, .curveEaseOut], animations: {cell.transform = CGAffineTransform.identity}, completion: nil)
+            delayCounter += 1
+        }
     }
 }
