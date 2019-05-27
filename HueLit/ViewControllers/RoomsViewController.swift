d536c98e06c902e9eb5181a6a5808d2cf7d90f53
Christian Persson <christian.lars.persson@gmail.com>
2019-05-27 13:41:57 +0200

    Add json parsing

--- HueLit/ViewControllers/RoomsViewController.swift
+++ HueLit/ViewControllers/RoomsViewController.swift
@@ -13,41 +13,60 @@ import SwiftyJSON
 class RoomsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var roomsTableView: UITableView!
-    let url = "http:192.168.1.225/api/CX0XuJlmCpBkjKepii0zJl6P3i7J77-dduoNjiTM/groups"
+    let url = "http:10.206.9.199/api/poV2ob5w8BJN0Vw7gQz8-VqXt883Qz46mwc2a6Pn/groups"
     var roomJSON : JSON? = JSON.null
     var roomInfo : [RoomInfo] = []
+    var test : [String : Any] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         roomsTableView.delegate = self
         roomsTableView.dataSource = self
         roomsTableView.backgroundColor = UIColor.darkGray
+        searchForRooms()
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        searchForRooms()
+//        roomsTableView.reloadData()
+//        print("in viewDidAppear: \(roomInfo.count)")
+//    }
+    
+    func searchForRooms() {
         Alamofire.request(url, method: .get)
             .responseJSON { response in
                 if response.result.isSuccess {
                     self.roomJSON = JSON(response.result.value!)
-//                    print(self.roomJSON!)
+                    print(self.roomJSON!)
                     for room in self.roomJSON! {
                         print(room.1["name"])
                         print(room.1["lights"])
                         print(room.1["action"])
+                        print(room.1["state"])
                         let someRoom = RoomInfo(roomName: room.1["name"].stringValue)
-                        self.roomInfo.append(someRoom)
-
-//                        let roomInfo = RoomInfo(roomName: room.1["name"].stringValue, lights: room.1["lights"].arrayValue, action: room["actions"])
+                        let actionJSON = JSON(room.1["action"])
+                        print(actionJSON)
+                        for (key, value) in actionJSON {
+                            self.test.map
+                            print("key: \(key)")
+                            print("JSON: \(value)")
+                        }
+                        print("xxxxxxxxxxxxxxxxxxx")
+//                        print(actionJSON)
+//                        let room = RoomInfo(roomName: room.1["name"].stringValue, lights: room.1["lights"].arrayObject, action: room.1["actions"].arrayValue)
+                         let room = RoomInfo(roomName: room.1["name"].stringValue)
+                        print("printing someroom: \(room)")
+                        self.roomInfo.append(room)
                     }
                     print("--------------")
                     print(self.roomInfo.count)
+                    self.roomsTableView.reloadData()
                 }
-            }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return roomInfo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -56,8 +75,8 @@ class RoomsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.cornerRadius = cell.frame.height / 4
         cell.backgroundColor = UIColor.lightGray
-        cell.roomNameLabel.text = "TypAvRum\(indexPath.row)"
-//        cell.roomNameLabel.text = roomInfo[indexPath.row].roomName
+//        cell.roomNameLabel.text = "TypAvRum\(indexPath.row)"
+        cell.roomNameLabel.text = roomInfo[indexPath.row].roomName
         cell.lightsInfoLabel.text = "Alla lampor är av"
         cell.lightSwitch.isOn = false
         return cell
@@ -70,26 +89,8 @@ class RoomsViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
 
 //http://192.168.1.225/api/CX0XuJlmCpBkjKepii0zJl6P3i7J77-dduoNjiTM/groups/1/action
+//875058
+//D66B18
+//
 
-//Alamofire.request(url, method: .get)
-//    .responseJSON { response in
-//        if response.result.isSuccess {
-//            self.bridgeJSON = JSON(response.result.value!)
-//            print(self.bridgeJSON!)
-//            print(self.bridgeJSON![0]["internalipaddress"])
-//            print(self.bridgeJSON![0]["id"])
-//
-//            for bridge in self.bridgeJSON! {
-//                let bridgeInfo = BridgeInfo(ip: bridge.1["internalipaddress"].stringValue, id: bridge.1["id"].stringValue)
-//                self.bridges.append(bridgeInfo)
-//                self.bridges.append(bridgeInfo)
-//                self.bridges.append(bridgeInfo)
-//                self.bridges.append(bridgeInfo)
-//
-//                print(self.bridges)
-//                if self.bridges.count > 0 {
-//                    self.perform(#selector(self.updateSearchUI), with: nil, afterDelay: 3)
-//                }
-//            }
-//        }
-//}
+//från fjället CX0XuJlmCpBkjKepii0zJl6P3i7J77-dduoNjiTM
