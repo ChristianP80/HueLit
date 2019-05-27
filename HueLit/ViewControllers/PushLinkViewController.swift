ca7bbaef9852cdafba3cbf1dd76d3c4fa9577b46
Christian Persson <christian.lars.persson@gmail.com>
2019-05-27 13:40:57 +0200

    Add timer and alert to viewcontroller

--- HueLit/ViewControllers/PushLinkViewController.swift
+++ HueLit/ViewControllers/PushLinkViewController.swift
@@ -11,6 +11,9 @@ import Alamofire
 import SwiftyJSON
 
 class PushLinkViewController: UIViewController {
+    
+    @IBOutlet weak var bridgeProgressView: UIProgressView!
+    
     let defaults = UserDefaults.standard
     var url = String()
     var bridge : BridgeInfo? = nil
@@ -25,14 +28,18 @@ class PushLinkViewController: UIViewController {
         getUser()
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        isLooping = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        countDownUntilTimeout()
+    }
+    
     func buildUrl() {
         let ip = defaults.string(forKey: "bridgeIp") ?? String()
         print(ip)
         url = "http://\(ip)/api"
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        isLooping = false
     }
     
     func getUser() {
@@ -46,30 +53,62 @@ class PushLinkViewController: UIViewController {
     
     @objc func checkIfPushLinkIsPressed() {
         print("this is the utl:\(url)")
-            Alamofire.request(url, method: .post, parameters: jsonBody, encoding: JSONEncoding.default)
-                .responseJSON { response in
-                    if response.result.isSuccess {
-                        let jsonResponse = JSON(response.result.value as Any)
-                        print("---------------------------------------")
-                        print(jsonResponse)
-                        print("---------------------------------------")
-                        if jsonResponse[0]["success"].exists() {
-                            print("contains success")
-                            self.user = jsonResponse[0]["success"]["username"].stringValue
-                            self.defaults.set(jsonResponse[0]["success"]["username"].stringValue, forKey: "bridgeUser")
-                            print(self.user!)
-                            let roomsVC = self.storyboard?.instantiateViewController(withIdentifier: "roomsVC") as! RoomsViewController
-                            self.present(roomsVC, animated: true, completion: nil)
-                        }
-                        if jsonResponse[0]["error"].exists() {
-                            self.getUser()
-                        }
-                    } else {
-                        print("Funkar inte")
-                        print(response.error.debugDescription)
+        Alamofire.request(url, method: .post, parameters: jsonBody, encoding: JSONEncoding.default)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    let jsonResponse = JSON(response.result.value as Any)
+                    print("---------------------------------------")
+                    print(jsonResponse)
+                    print("---------------------------------------")
+                    if jsonResponse[0]["success"].exists() {
+                        print("contains success")
+                        self.user = jsonResponse[0]["success"]["username"].stringValue
+                        self.defaults.set(jsonResponse[0]["success"]["username"].stringValue, forKey: "bridgeUser")
+                        print(self.user!)
+                        let roomsVC = self.storyboard?.instantiateViewController(withIdentifier: "roomsVC") as! RoomsViewController
+                        self.present(roomsVC, animated: true, completion: nil)
                     }
-            }
+                    if jsonResponse[0]["error"].exists() {
+                        self.getUser()
+                    }
+                } else {
+                    print("Funkar inte")
+                    print(response.error.debugDescription)
+                }
+        }
     }
+    
+    func timeOutAlert(){
+        let alertController = UIAlertController(title: "Timeout", message: "You haven´t pressed the pushlink on the bridge", preferredStyle: .alert)
+        let retryAction = UIAlertAction(title: "Retry", style: .default) { (action) in
+            self.bridgeProgressView.progress = 1.0
+            self.countDownUntilTimeout()
+            self.getUser()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(retryAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func countDownUntilTimeout() {
+        let timeout : Float = 30
+        var timeLeft = timeout
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            timeLeft -= 1
+            let progress = timeLeft / timeout
+            self.bridgeProgressView.progress = progress
+            
+            if timeLeft == 0{
+                self.timeOutAlert()
+                timer.invalidate()
+            }
+        }
+    }
+    
 }
 
 
