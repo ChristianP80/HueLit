a525da1335e706b6582ad1b324078257453da6fd
Christian Persson <christian.lars.persson@gmail.com>
2019-05-31 12:22:32 +0200

    Add new cell for room setup

--- HueLit/Cells/RoomSetupCell.swift
+++ HueLit/Cells/RoomSetupCell.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class RoomSetupCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var roomImageView: UIImageView!
+    @IBOutlet weak var roomNameLabel: UILabel!
+    @IBOutlet weak var numberOfLightsLabel: UILabel!
+    
+    var room : (key: String, val: RoomInfo)!
+    
+    func setCellLayout(cell: RoomSetupCell) {
+        cell.layer.borderWidth = 5.0
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.cornerRadius = cell.frame.height / 4
+        cell.backgroundColor = UIColor.lightGray
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setRoomInfo(roomInfo: (key: String, val: RoomInfo)) {
+        self.room = roomInfo
+        roomNameLabel.text = room.val.name
+        numberOfLightsLabel.text = "\(room.val.lights.count) lights available"
     }
-
 }
