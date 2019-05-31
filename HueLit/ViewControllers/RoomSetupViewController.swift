fd81d57235cc34a6e8d87fc6c53b2c2e24a98abb
Christian Persson <christian.lars.persson@gmail.com>
2019-05-31 12:24:12 +0200

    Add new viewcontroller to storyboard

--- HueLit/ViewControllers/RoomSetupViewController.swift
+++ HueLit/ViewControllers/RoomSetupViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
-class RoomSetupViewController: UIViewController {
+class RoomSetupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var roomSetupNavBar: UINavigationBar!
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var roomTableView: UITableView!
+    
+    var roomArray : [(key: String, val: RoomInfo)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "TESTAR"
+        roomTableView.delegate = self
+        roomTableView.dataSource = self
+        roomTableView.estimatedRowHeight = 80
+        roomTableView.rowHeight = 60
+        roomSetupNavBar.topItem?.title = "Room Setup"
+        createButton.backgroundColor = UIColor.cyan
+        createButton.layer.cornerRadius = 5
+        print(roomArray[1])
+        print(roomArray.count)    }
+    
+    @IBAction func closeRoomSetup(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func createNewRoomClicked(_ sender: Any) {
+    }
+    
+    func getRoomSetup() {
+        
+    }
+    
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return roomArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = roomTableView.dequeueReusableCell(withIdentifier: "roomSetupCell") as! RoomSetupCell
+        cell.setCellLayout(cell: cell)
+        cell.setRoomInfo(roomInfo: roomArray[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "AVAILABLE ROOMS"
+    }
+    
 }
