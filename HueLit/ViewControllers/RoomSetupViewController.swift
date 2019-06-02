7e25343400c604e3d2887e481eef5d5a2e86a36f
Christian Persson <christian.lars.persson@gmail.com>
2019-06-02 13:13:04 +0200

    Get all rooms using URLSession

--- HueLit/ViewControllers/RoomSetupViewController.swift
+++ HueLit/ViewControllers/RoomSetupViewController.swift
@@ -14,7 +14,11 @@ class RoomSetupViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var roomTableView: UITableView!
     
+    let jsonUrl = "http:192.168.1.225/api/mooY-Ctmw5-YSLO4m0Uyw30BBAvzjJYInxzmCzA8/groups"
+    var roomInfo : [String:RoomInfo] = [:]
     var roomArray : [(key: String, val: RoomInfo)] = []
+    let bridgeUser = UserDefaults.standard.string(forKey: "bridgeUser")
+    let bridgeIp = UserDefaults.standard.string(forKey: "bridgeIp")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +28,8 @@ class RoomSetupViewController: UIViewController, UITableViewDelegate, UITableVie
         roomTableView.rowHeight = 60
         roomSetupNavBar.topItem?.title = "Room Setup"
         createButton.layer.cornerRadius = 5
-        print(roomArray[1])
-        print(roomArray.count)    }
+        getRoomSetup()
+    }
     
     @IBAction func closeRoomSetup(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -37,7 +41,26 @@ class RoomSetupViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func getRoomSetup() {
-        
+        guard let url = URL(string: jsonUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            do {
+                self.roomInfo = try JSONDecoder().decode([String:RoomInfo].self, from: data)
+                self.roomArray = self.roomInfo.map{$0}
+                self.roomArray = self.roomArray.sorted(by: { $0.0 < $1.0})
+                print("kommer hit")
+                print(type(of: self.roomArray))
+                print(self.roomArray.count)
+                for item in self.roomArray {
+                    print(item)
+                }
+            } catch let error {
+                print("Error:", error)
+            }
+            DispatchQueue.main.async {
+                self.roomTableView.reloadData()
+            }
+            }.resume()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
