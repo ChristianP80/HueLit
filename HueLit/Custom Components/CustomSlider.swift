84b36a446e37ef690705f9794d3b6903b407bbcf
Christian Persson <christian.lars.persson@gmail.com>
2019-06-03 13:11:51 +0200

    Modify cell height

---
+++ HueLit/Custom Components/CustomSlider.swift
@@ -0,0 +1,21 @@
+//
+//  CustomSlider.swift
+//  HueLit
+//
+//  Created by Christian Persson on 2019-06-03.
+//  Copyright © 2019 Christian Persson. All rights reserved.
+//
+
+import UIKit
+
+class CustomSlider: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
