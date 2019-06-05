44ed8b8993e349034b032c8d3f73724252e76ef3
Christian Persson <christian.lars.persson@gmail.com>
2019-06-05 23:31:32 +0200

    new custom component to be able to create rounded buttons

--- HueLit/CustomComponents/CustomRoundButton.swift
+++ HueLit/CustomComponents/CustomRoundButton.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019 Christian Persson. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
