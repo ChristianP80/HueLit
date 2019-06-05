4e49b5c248fafef0b8013110b4744d53c19b020c
Christian Persson <christian.lars.persson@gmail.com>
2019-06-05 23:26:48 +0200

    Add new white assets for rooms instead of black

---
+++ HueLit/CustomComponents/CustomRoundButton.swift
@@ -0,0 +1,9 @@
+//
+//  CustomRoundButton.swift
+//  HueLit
+//
+//  Created by Christian Persson on 2019-06-04.
+//  Copyright © 2019 Christian Persson. All rights reserved.
+//
+
+import Foundation
---
+++ HueLit/ViewControllers/SelectedRoomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectedRoomViewController.swift
+//  HueLit
+//
+//  Created by Christian Persson on 2019-06-04.
+//  Copyright © 2019 Christian Persson. All rights reserved.
+//
+
+import UIKit
+
+class SelectedRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
