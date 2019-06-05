3ddea7ae9ef78acd664544d71cb15f322582d98a
Christian Persson <christian.lars.persson@gmail.com>
2019-06-05 23:28:03 +0200

    Add change color request using URLSession

--- HueLit/ViewControllers/SelectedRoomViewController.swift
+++ HueLit/ViewControllers/SelectedRoomViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class SelectedRoomViewController: UIViewController {
 
+    var room : (key: String, val: RoomInfo)? = nil
+    let changeStateURL = "http:192.168.1.225/api/mooY-Ctmw5-YSLO4m0Uyw30BBAvzjJYInxzmCzA8/groups/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(room!)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeColor(_ sender: RoundButton) {
+        var hue : Int = 0
+        switch sender.tag {
+        case 1:
+            hue = 65535
+        case 2:
+            hue = 25500
+        case 3:
+            hue = 46920
+        default:
+            return
+        }
+        print(hue)
+        
+        
+        let jsonUrl = "http:192.168.1.225/api/u2q1iWISCdcW2bOee7ixyRlli9Sd5p-G4PM0ZNUI/groups/\(room!.key)/action"
+        let body = ["hue": hue,
+                    "on": true,
+                    "bri": room!.val.action!.bri] as [String : Any]
+        print(body)
+        guard let url = URL(string: jsonUrl) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let jsonData = try! JSONSerialization.data(withJSONObject: body, options: [])
+        URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
+            guard let response = response as? HTTPURLResponse else { return }
+            if (200...209).contains(response.statusCode) {
+                
+            }
+        }.resume()
     }
-    */
-
 }
+
